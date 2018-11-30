b32bf6a4db477b5f247e3b409a73beb0031baedb
ngwind <ngwind@foxmail.com>
2018-11-30 19:40:59 +0800

    使用通用视图generic来代替index、detail和results方法

--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        "latest_question_list" : latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         "latest_question_list" : latest_question_list,
+#     }
+#     return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk= question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#   return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
 
 def vote(request,question_id):
