6aad972c299a9630b93f61108ae5bda023c81678
ngwind <ngwind@foxmail.com>
2018-11-30 16:47:48 +0800

    把render_to_response()换成render()，修复403问题

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response,get_object_or_404,render
+from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
 from django.urls import reverse
 
@@ -9,12 +9,12 @@ def index(request):
     context = {
         "latest_question_list" : latest_question_list,
     }
-    return render_to_response("polls/index.html", context)
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render_to_response("polls/detail.html", {"question": question})
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -27,11 +27,11 @@ def vote(request,question_id):
     try:
         select_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoseNotExist):
-        return render_to_response('polls/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
-        select_choice.vote += 1
+        select_choice.votes += 1
         select_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
