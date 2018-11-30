a2cea75c5ee25b08369a8830eca4a011f96af054
ngwind <ngwind@foxmail.com>
2018-11-30 20:39:44 +0800

    自定义后台管理界面

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date', 'id']
+    fields = ['pub_date', 'question_text']
+    list_filter = ['pub_date']
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice, ChoiceInline)
